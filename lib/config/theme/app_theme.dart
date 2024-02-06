985bcc284cc3d71859b27051e9905aa97986e87e
macdo <martincafiero@gmail.com>
2024-02-06 17:36:32 -0300

    archivos modificados inicialmente

---
+++ lib/config/theme/app_theme.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Apptheme {
+
+ThemeData getTheme() {
+  return ThemeData(
+    useMaterial3: true,
+    colorSchemeSeed: const Color(0xFF2862F5),
+  );
+
+}
+}
+
