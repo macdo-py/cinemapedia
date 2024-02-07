1ac018e6980d17ec6ba6b4f180d46f08a9f1ada1
macdo <martincafiero@gmail.com>
2024-02-07 08:44:23 -0300

    quicktype.io original code

--- README.md
+++ README.md
@@ -9,3 +9,9 @@ A new Flutter project.
 
 3 agregar dependencia flutter_dotenv
 4 agregar .env en pubsec.yaml  assets
+
+5 agregar dependencia dio
+
+6 tomar la respuesta desde postman de movies_playing copiar en quicktype.io y crear con el codigo:
+    lib/infrastructure/models/moviedb/moviedb_response.dart
+    
---
+++ lib/infrastructure/datasources/moviedb_datasource.dart
@@ -0,0 +1,20 @@
+import 'package:cinemapedia/config/constants/environment.dart';
+import 'package:cinemapedia/domain/datasources/movies_datasource.dart';
+import 'package:cinemapedia/domain/entities/movie.dart';
+import 'package:dio/dio.dart';
+
+class MoviesDbDataSource extends MoviesDataSource {
+  final dio = Dio(
+      BaseOptions(baseUrl: 'https://api.themoviedb.org/3', queryParameters: {
+    'api_key': Environment.theMovieDbKey,
+    'language': 'es-ES',
+  }));
+
+  @override
+  Future<List<Movie>> getPopularMovies({int page = 1}) async {
+    final respones = await dio.get('/movie/now_playing');
+    final List<Movie> movies = [];
+
+    return [];
+  }
+}
