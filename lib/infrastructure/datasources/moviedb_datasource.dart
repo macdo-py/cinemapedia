ce335c5380ffb12905e2fb97feb68b481e3e0991
macdo <martincafiero@gmail.com>
2024-02-07 09:43:09 -0300

    implementacion datasource

--- lib/infrastructure/datasources/moviedb_datasource.dart
+++ lib/infrastructure/datasources/moviedb_datasource.dart
@@ -1,7 +1,9 @@
+import 'package:dio/dio.dart';
 import 'package:cinemapedia/config/constants/environment.dart';
 import 'package:cinemapedia/domain/datasources/movies_datasource.dart';
 import 'package:cinemapedia/domain/entities/movie.dart';
-import 'package:dio/dio.dart';
+import 'package:cinemapedia/infrastructure/mappers/movie_mapper.dart';
+import 'package:cinemapedia/infrastructure/models/moviedb/moviedb_response.dart';
 
 class MoviesDbDataSource extends MoviesDataSource {
   final dio = Dio(
@@ -12,10 +14,15 @@ class MoviesDbDataSource extends MoviesDataSource {
 
   @override
   Future<List<Movie>> getPopularMovies({int page = 1}) async {
-    final respones = await dio.get('/movie/now_playing');
-    
-    final List<Movie> movies = [];
+    final response = await dio.get('/movie/now_playing');
+    final movieDBResponse = MovieDbResponse.fromJson(response.data);
 
-    return [];
+
+    final List<Movie> movies = movieDBResponse.results.map(
+      (movieDB) => MovieMapper.movieDBToEntity(movieDB)).toList();
+      
+      
+
+    return movies;
   }
 }
