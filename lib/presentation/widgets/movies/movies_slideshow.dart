9016df286560bf27e291ea1a0fb9fb0c270c4702
macdo <martincafiero@gmail.com>
2024-02-07 17:11:46 -0300

    swiper con imgs

--- lib/presentation/widgets/movies/movies_slideshow.dart
+++ lib/presentation/widgets/movies/movies_slideshow.dart
@@ -1,3 +1,4 @@
+import 'package:animate_do/animate_do.dart';
 import 'package:card_swiper/card_swiper.dart';
 import 'package:cinemapedia/domain/entities/movie.dart';
 import 'package:flutter/material.dart';
@@ -10,32 +11,51 @@ class MoviesSlideshow extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SizedBox(
-      height: 210,
-      width: double.infinity,
-      child: Swiper(
-        viewportFraction: 0.8,
-        scale: 0.9,
-        autoplay: true,
-        itemCount: movies.length ,
-        itemBuilder: (context, index) {
-          return _Slide(movie: movies[index]);
-        },
-        )
-        
-        
-        
-    );
+        height: 210,
+        width: double.infinity,
+        child: Swiper(
+          viewportFraction: 0.8,
+          scale: 0.9,
+          autoplay: true,
+          itemCount: movies.length,
+          itemBuilder: (context, index) {
+            return _Slide(movie: movies[index]);
+          },
+        ));
   }
 }
 
-
 class _Slide extends StatelessWidget {
-
   final Movie movie;
   const _Slide({required this.movie});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    final decoration = BoxDecoration(
+      borderRadius: BorderRadius.circular(20),
+      boxShadow: const [
+        BoxShadow(
+          color: Colors.black26,
+          blurRadius: 10,
+          offset: Offset(0, 10),
+        )
+      ],
+    );
+
+    return Padding(
+        padding: const EdgeInsets.only(bottom: 30),
+        child: DecoratedBox(
+          decoration: decoration,
+          child: ClipRRect(
+              borderRadius: BorderRadius.circular(20),
+              child: Image.network(movie.backdropPath, fit: BoxFit.cover,
+                  loadingBuilder: (context, child, loadingProgress) {
+                if (loadingProgress != null) {
+                  return const DecoratedBox(
+                      decoration: BoxDecoration(color: Colors.black));
+                }
+                return FadeIn(child: child);
+              })),
+        ));
   }
 }
