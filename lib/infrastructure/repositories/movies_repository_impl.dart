f5d63f6b5af3d9cb91648d4e31612b83b8dc3f6b
macdo <martincafiero@gmail.com>
2024-02-07 09:54:15 -0300

    repo_impl

---
+++ lib/infrastructure/repositories/movies_repository_impl.dart
@@ -0,0 +1,19 @@
+
+
+
+
+import 'package:cinemapedia/domain/datasources/movies_datasource.dart';
+import 'package:cinemapedia/domain/entities/movie.dart';
+import 'package:cinemapedia/domain/repositories/movies_repository.dart';
+
+class MovieRepositoryImpl extends MoviesRepository {
+
+final MoviesDataSource datasource;
+
+  MovieRepositoryImpl(this.datasource);
+
+@override
+  Future<List<Movie>> getPopularMovies({int page = 1}) {
+    return datasource.getPopularMovies(page: page);
+  }
+}
