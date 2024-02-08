80e6b2b70976038e89ca8623af4ea5635cf11e7b
macdo <martincafiero@gmail.com>
2024-02-08 17:58:06 -0300

    movieDetails - local cache

---
+++ lib/presentation/providers/movies/movie_info_provider.dart
@@ -0,0 +1,24 @@
+import 'package:cinemapedia/presentation/providers/providers.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:cinemapedia/domain/entities/movie.dart';
+
+final movieInfoProvider =
+    StateNotifierProvider<MovieMapNotifier, Map<String, Movie>>((ref) {
+  final moviesRepository = ref.watch(movieRepositoryProvider);
+  return MovieMapNotifier(getMovie: moviesRepository.getMovieById);
+});
+
+typedef GetMovieCallback = Future<Movie> Function(String movieId);
+
+class MovieMapNotifier extends StateNotifier<Map<String, Movie>> {
+  final GetMovieCallback getMovie;
+
+  MovieMapNotifier({required this.getMovie}) : super({});
+
+  Future<void> loadMovie(String movieId) async {
+    if (state[movieId] != null) return;
+    print('realizando peticion http $movieId');
+    final movie = await getMovie(movieId);
+    state = {...state, movieId: movie};
+  }
+}
--- lib/presentation/screens/movies/movie_screen.dart
+++ lib/presentation/screens/movies/movie_screen.dart
@@ -1,17 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:cinemapedia/domain/entities/movie.dart';
+import 'package:cinemapedia/presentation/providers/providers.dart';
 
-class MovieScreen extends StatelessWidget {
+class MovieScreen extends ConsumerStatefulWidget {
   static const name = 'movie-screen';
   final String movieId;
 
   const MovieScreen({super.key, required this.movieId});
 
   @override
+  MovieScreenState createState() => MovieScreenState();
+}
+
+class MovieScreenState extends ConsumerState<MovieScreen> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    ref.read(movieInfoProvider.notifier).loadMovie(widget.movieId);
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return  Scaffold(
+    final Movie? movie = ref.watch(movieInfoProvider)[widget.movieId];
+
+    if (movie == null) {
+      return const Center(
+        child: CircularProgressIndicator(),
+      );
+    }
+
+    return Scaffold(
       appBar: AppBar(
-        title: Text('MovieID: $movieId'),
-        ),
+        title: Text('MovieID: ${widget.movieId}'),
+      ),
     );
   }
 }
