fb1e97450d490725a0b45a66f634579c168ae270
macdo <martincafiero@gmail.com>
2024-02-09 15:17:40 -0300

    delegate buildActions buildLeading

--- lib/presentation/delegates/search_movie_delegate.dart
+++ lib/presentation/delegates/search_movie_delegate.dart
@@ -1,31 +1,39 @@
+import 'package:animate_do/animate_do.dart';
+import 'package:cinemapedia/domain/entities/movie.dart';
 import 'package:flutter/material.dart';
 
-class SearchMovieDelegate extends SearchDelegate {
-  
+class SearchMovieDelegate extends SearchDelegate<Movie?> {
   @override
   String get searchFieldLabel => 'Search for movies';
-  
-  
+
   @override
   List<Widget>? buildActions(BuildContext context) {
-    return [Text('buildActions')];
+    //  print('query: $query');
+    return [
+      if (query.isNotEmpty)
+      FadeIn(
+        animate: query.isNotEmpty,
+        child: 
+          IconButton(onPressed: () => query = '', icon: const Icon(Icons.clear)
+          )
+          ),
+    ];
   }
 
   @override
   Widget? buildLeading(BuildContext context) {
-    return Text('buildLeading');
+    return  IconButton(
+          onPressed: () => close(context, null),
+          icon: const Icon(Icons.arrow_back_ios_new_rounded));
   }
 
   @override
   Widget buildResults(BuildContext context) {
- return Text('buildResults');
+    return Text('buildResults');
   }
 
   @override
   Widget buildSuggestions(BuildContext context) {
     return Text('buildSuggestions');
   }
-
-
-
 }
