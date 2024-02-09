e3ba26c5bcd2b107439ae49cfd34c0b29282cba5
macdo <martincafiero@gmail.com>
2024-02-09 12:43:22 -0300

    mostar los actores en pantalla

--- lib/presentation/screens/movies/movie_screen.dart
+++ lib/presentation/screens/movies/movie_screen.dart
@@ -1,3 +1,4 @@
+import 'package:cinemapedia/domain/entities/actor.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:cinemapedia/domain/entities/movie.dart';
@@ -19,12 +20,13 @@ class MovieScreenState extends ConsumerState<MovieScreen> {
     // TODO: implement initState
     super.initState();
     ref.read(movieInfoProvider.notifier).loadMovie(widget.movieId);
-    ref.read(actorByMovieProvider.notifier).loadActors(widget.movieId);
+    // ref.read(actorByMovieProvider.notifier).loadActors(widget.movieId);
   }
 
   @override
   Widget build(BuildContext context) {
     final Movie? movie = ref.watch(movieInfoProvider)[widget.movieId];
+    // final Actor? actors = ref.watch(actorByMovieProvider)[widget.movieId];
 
     if (movie == null) {
       return const Center(
@@ -61,63 +63,106 @@ class _MovieDetails extends StatelessWidget {
       children: [
         Padding(
           padding: const EdgeInsets.all(8),
-          child: Row(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            
-            children: [
-              ClipRRect(
-                borderRadius: BorderRadius.circular(20),
-                child: Image.network(
-                  movie.posterPath,
-                  fit: BoxFit.cover,
-                   width: size.width * 0.3,
-                   height: size.height * 0.3,
-                ),
-                
+          child: Row(crossAxisAlignment: CrossAxisAlignment.start, children: [
+            ClipRRect(
+              borderRadius: BorderRadius.circular(20),
+              child: Image.network(
+                movie.posterPath,
+                fit: BoxFit.cover,
+                width: size.width * 0.3,
+                height: size.height * 0.3,
               ),
-               SizedBox(
-                width: 10,
-                
-                ),
- 
-               SizedBox(
-                width: (size.width - 40) * 0.7,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(movie.title, style: textStyle.titleLarge),
-                    Text(movie.overview, style: textStyle.bodyMedium),
-                  ],
-                ),
-                 ),
-
-            ]),
-          ),
-
-
+            ),
+            SizedBox(
+              width: 10,
+            ),
+            SizedBox(
+              width: (size.width - 40) * 0.7,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  //Text(movie.title, style: textStyle.titleLarge),
+                  Text(movie.overview, style: textStyle.bodyMedium),
+                ],
+              ),
+            ),
+          ]),
+        ),
 
         //TODO Agregar genero
-        Padding(padding: const EdgeInsets.all(8),
-        child: Wrap(
-          children: [...movie.genreIds.map((gender) => Container(
-            margin: const EdgeInsets.only(right: 10),
-            child: Chip(
-              label: Text(gender),
-              shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20),),
+        Padding(
+          padding: const EdgeInsets.all(8),
+          child: Wrap(
+            children: [
+              ...movie.genreIds.map((gender) => Container(
+                    margin: const EdgeInsets.only(right: 10),
+                    child: Chip(
+                      label: Text(gender),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                    ),
+                  ))
+            ],
+          ),
+        ),
+
+        // TODO: mostar actores
+        _ActorsByMovie(movieId: movie.id.toString()),
+
+        const SizedBox(height: 50),
+      ],
+    );
+  }
+}
+
+class _ActorsByMovie extends ConsumerWidget {
+  final String movieId;
+
+  const _ActorsByMovie({required this.movieId});
+
+  @override
+  Widget build(BuildContext context, ref) {
+    final actorsByMovie = ref.watch(actorByMovieProvider);
+    if (actorsByMovie[movieId] == null) {
+      return const Center(
+        child: CircularProgressIndicator(),
+      );
+
+    }
+    final actors = actorsByMovie[movieId]!;
+
+    return  SizedBox(
+      height: 300,
+      child: ListView.builder(
+        scrollDirection: Axis.horizontal,
+        itemCount: actors.length,
+        itemBuilder: (context, index) {
+          final actor = actors[index];
+
+          return Container(
+            margin: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(20),
+                  child: Image.network(
+                    actor.profilePath,
+                    fit: BoxFit.cover,
+                    width: 180,
+                    height: 130,
+                  ),
+                ),
+                Text(actor.name, maxLines: 2,),
+                Text(actor.character ?? '', 
+                    maxLines: 2,
+                    style: const TextStyle(fontWeight: FontWeight.bold), overflow: TextOverflow.ellipsis,),
+              ],
             ),
-          ))
-          ],
+          );
+        },
         ),
-        ),
-
-
-
-
-
-
-         // TODO: mostar actores 
-        const SizedBox(height: 100),
-        ],
     );
   }
 }
